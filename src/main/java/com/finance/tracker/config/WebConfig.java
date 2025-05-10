09012f942a5d14080add002de57a056b9f16fd08
CesJauregui <cjaureguisaavedra@gmail.com>
2025-05-10 13:21:48 -0500

    Habilitando la url del front como variable de entorno en el CORS

--- src/main/java/com/finance/tracker/config/WebConfig.java
+++ src/main/java/com/finance/tracker/config/WebConfig.java
@@ -1,14 +1,18 @@
 package com.finance.tracker.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${frontend.base.url}")
+    private String frontEndBaseUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // aplica a todas las rutas
-                .allowedOrigins("http://localhost:5173") // frontend en Next.js
+                .allowedOrigins(frontEndBaseUrl,"http://localhost:5173") // frontend en Next.js
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // métodos permitidos
                 .allowedHeaders("*"); // permite todos los headers
     }
